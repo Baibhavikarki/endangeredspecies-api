4f675498d73f140e6a26b3032373f04226b619af
Baibhavikarki <baibhavikarki325@gmail.com>
2023-02-28 23:06:54 -0500

    npm install cors

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
+const cors = require('cors');
+
 var app = express();
+app.use(cors({ origin: 'http://localhost:3000' }));
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
