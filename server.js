4ef99c769c1f8249b29a295cba0dad1ff60f76d7
Baibhavikarki <baibhavikarki325@gmail.com>
2023-02-28 21:49:49 -0500

    database connection with mongodb

--- server.js
+++ server.js
@@ -1,7 +1,32 @@
 var express = require("express");
 var app = express();
 
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 var HTTP_PORT = process.env.PORT || 8080;
+
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb+srv://OpenAI:2RpW2oM2lVdPFyHq@cluster0.fyqnoa9.mongodb.net/endangered_species';
+ 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+let db;
+async function connect() {
+  try {
+    await client.connect();
+    console.log('Connected to the database');
+    db = client.db('endangered_species');
+  } catch (err) {
+    console.log('Error connecting to the database', err);
+  }
+}
+
+connect();
+
+
+app.listen(HTTP_PORT, onHttpStart);
 
 
 function onHttpStart() {
@@ -9,9 +34,26 @@ function onHttpStart() {
 }
 
 
-app.get("/", function(req,res){
-    res.send("Hello World");
+app.get("/individuals", async (req,res) => {
+  try {
+ 
+    const result = await db.collection('individuals').find().toArray();
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error retrieving data');
+  }
 });
 
 
-app.listen(HTTP_PORT, onHttpStart);
+app.get("/sightings/:animalId", async (req,res) => {
+  try {
+    const animalId = parseInt(req.params.animalId);
+    console.log(animalId)
+    const result = await db.collection('sightings').find({animal_id: animalId}).toArray();
+    res.send(result);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send('Error retrieving data');
+  }
+});
